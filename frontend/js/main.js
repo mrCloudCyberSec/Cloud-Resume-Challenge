930047ebae89d73b5120773e2b1efd19aafd3cce
mrCloudCyberSec <mrufincyber@gmail.com>
2024-08-16 03:39:56 -0400

    modified:   frontend/index.html 	modified:   frontend/js/main.js

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -295,30 +295,6 @@
 
     }; // end ssBackToTop
 
-
-    /* Counter
-    * ------------------------------------------------------ */
-    window.addEventListener('DOMContentLoaded', (event) =>{
-        getVisitCount();
-    })
-    
-    const functionApi = '';
-
-    const getVisitCount = () => {
-        let count = 30;
-        fetch(functionApi).then(response => {
-            return response.json()
-        }).then(response =>{
-            console.log("Website called function API.");
-            count = response.count;
-            document.getElementById("counter").innerText = count;
-        }).catch(function(error){
-            console.log(error);
-        });
-        return count;
-    }// end counter
-
-
    /* initialize
     * ------------------------------------------------------ */
     (function ssInit() {
